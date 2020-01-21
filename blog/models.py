78e8c17cb08651f37fab2930b048b6ba1488c1af
Nikhil Mulchandani <nikmul19@gmail.com>
2020-01-21 12:44:12 +0530

    First commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+	name = models.CharField(max_length=120)
+	created_on = models.DateTimeField(auto_now_add=True)
+	
+
+class Article(models.Model):
+	blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
+	created_on = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=120)
+	draft = models.BooleanField(default= False)
+
+
+
+
